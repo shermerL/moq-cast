df949f057a3e44c633637ab691cd1e4e0d9fee76
shermerL <86936692+shermerL@users.noreply.github.com>
2026-08-23 16:41:57 +0800

    chore: release v0.4.1-dev.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.moqandroid"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.4.0"
+        versionCode = 7
+        versionName = "0.4.1-dev.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
